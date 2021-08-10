82b3ebe6bee88e9d00091abdbe0deed2f1565c21
Sebijk <github@sebijk.net>
2021-08-10 13:56:47 +0200

    add url validation

--- upload/deref.php
+++ upload/deref.php
@@ -16,6 +16,19 @@ if($sepPos !== false)
 	$targetURL = substr($url, $sepPos+1);
 }
 
+# URL validation from https://websolutionstuff.com/post/how-to-validate-url-in-php-with-regex
+$regex = "((https?|ftp)\:\/\/)?";
+$regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?";
+$regex .= "([a-z0-9-.]*)\.([a-z]{2,3})";
+$regex .= "(\:[0-9]{2,5})?";
+$regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?";
+$regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?";
+$regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?";
+
+if (!preg_match("/^$regex$/i", $targetURL)) {
+	$targetURL="";
+ }
+
 eval("\$template_deref = \"".$templates->get("deref", 1, 0)."\";");
 echo $template_deref;
 ?>
